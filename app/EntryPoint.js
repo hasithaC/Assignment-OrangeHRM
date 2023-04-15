81f3bc7bd348395091456ec35cd703a2c56a82af
hasitha_c <hasitha_c@epiclanka.net>
2023-04-15 19:46:04 +0530

    Set up app entry point

---
+++ app/EntryPoint.js
@@ -0,0 +1,13 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import Root from './Root';
+
+const EntryPoint = () => {
+  return (
+    <View style={{flex: 1}}>
+      <Root />
+    </View>
+  );
+};
+
+export default EntryPoint;
---
+++ app/Root.js
@@ -0,0 +1,14 @@
+import {StatusBar, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const Root = () => {
+  return <View style={styles.screen}></View>;
+};
+
+export default Root;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+});
